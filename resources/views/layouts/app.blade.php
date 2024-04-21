24e601418b0b6ef99427d219bbfbca2c0f7cac4f
M. Iqbal Effendi <iqbaleff214@gmail.com>
2024-04-21 19:26:35 +0800

    update content section for template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,7 +66,10 @@
           <!-- Content wrapper -->
           <div class="content-wrapper">
             <!-- Content -->
-            @yield('content')
+
+            <div class="container-xxl flex-grow-1 container-p-y">
+                @yield('content')
+            </div>
             <!-- / Content -->
 
             <!-- Footer -->
