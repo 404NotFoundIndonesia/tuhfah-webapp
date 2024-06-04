3910252c0e6561fba818eed3b390ddb68c7a3df6
M. Iqbal Effendi <iqbaleff214@gmail.com>
2024-06-04 13:58:52 +0800

    generate student id number when it is empty

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -39,7 +39,10 @@ class Student extends Model
         parent::boot();
         static::creating(function (Student $student) {
             if (is_null($student->student_id_number)) {
-                $student->student_id_number = time().random_int(0, 1000).time();
+                $studentIdNumber = $student->gender === 'male' ? 'I' : 'A';
+                $studentIdNumber .= now()->toHijri()->isoFormat('YYMM');
+                $number = Student::where('student_id_number', 'LIKE', $studentIdNumber . '%')->count() + 1;
+                $student->student_id_number = $studentIdNumber . str_pad($number, 3, '0',STR_PAD_LEFT);
             }
         });
         static::deleting(function (Student $student) {
